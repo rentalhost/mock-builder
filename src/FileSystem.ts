c0cbc61b5df5092719bbf6a81f79d56af8e19298
David Rodrigues <david.rodrigues@rentalhost.net>
2022-11-27 16:10:42 -0300

    MockBuilder: added support to "zod" format;

---
+++ src/FileSystem.ts
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+
+export default class FileSystem
+{
+    static writeSync(file: string, data: string)
+    {
+        fs.mkdirSync(path.dirname(file), { recursive: true });
+        fs.writeFileSync(file, data);
+    }
+}
--- src/MockBuilder.ts
+++ src/MockBuilder.ts
@@ -1,10 +1,21 @@
 import fs from 'fs';
 
 import { NextApiRequest, NextApiResponse } from 'next';
+import { ZodSchema } from 'zod';
+
+import FileSystem from '@src/FileSystem';
+
+interface DefineOptionsInterface
+{
+    format?: ZodSchema;
+}
 
 export default class MockBuilder
 {
-    private static processGet(path: string, response: NextApiResponse)
+    private static processGet(
+        path: string,
+        response: NextApiResponse
+    )
     {
         if (!fs.existsSync(path)) {
             return response.status(404).json({ error: 'file not found' });
@@ -13,16 +24,37 @@ export default class MockBuilder
         return response.status(200).send(fs.readFileSync(path).toString());
     }
 
-    private static processPost(path: string, request: NextApiRequest, response: NextApiResponse)
+    private static processPost(
+        path: string,
+        request: NextApiRequest,
+        response: NextApiResponse,
+        options?: DefineOptionsInterface
+    )
     {
-        fs.writeFileSync(path, JSON.stringify(request.body));
+        let requestBody = request.body;
 
-        return response.status(200).send(request.body);
+        if (options?.format) {
+            const parse = options.format.safeParse(requestBody);
+
+            if (!parse.success) {
+                return response.status(400).json({
+                    error:  'the data sent has an invalid format',
+                    issues: parse.error.issues
+                });
+            }
+
+            requestBody = parse.data;
+        }
+
+        FileSystem.writeSync(path, JSON.stringify(requestBody));
+
+        return response.status(200).send(requestBody);
     }
 
     static define(
         request: NextApiRequest,
-        response: NextApiResponse
+        response: NextApiResponse,
+        options?: DefineOptionsInterface
     )
     {
         const path = `./storage/${ request.url?.slice(5) }.json`;
@@ -32,9 +64,9 @@ export default class MockBuilder
         }
 
         if (request.method === 'POST') {
-            return this.processPost(path, request, response);
+            return this.processPost(path, request, response, options);
         }
 
-        return response.status(400).json({ error: 'not implemented' });
+        return response.status(501).json({ error: 'not implemented' });
     }
 }
